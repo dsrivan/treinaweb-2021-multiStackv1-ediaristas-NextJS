a37f77211e0dff7bb1dd3e08ce45a6b204a3f6b3
Ivan da Silva Rosa <iivan.si@msn.com>
2021-06-12 01:08:10 -0300

    Aula 2 - parcialmente finalizada

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,14 @@
 import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironment";
 import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
 import UserInformation from "ui/components/data-display/UserInformation/UserInformation";
+import TextFieldMask from "ui/components/inputs/TextFieldMask/TextFieldMask";
+import { Button, Typography, Container } from "@material-ui/core";
+import {
+  FormElementsContainer,
+  ProfissionaisPaper,
+  ProfissionaisContainer,
+} from "ui/styles/pages/index.style";
+import useIndex from "data/hooks/pages/useIndex.page";
 
 export default function Home() {
   return (
@@ -13,12 +21,67 @@ export default function Home() {
         }
       />
 
-      <UserInformation
-        picture={"https://github.com/dsrivan.png"}
-        name={"Ivan Rosa"}
-        rating={4}
-        description={"Cosmorama"}
-      />
+      <Container>
+        <FormElementsContainer>
+          <TextFieldMask
+            mask={"99.999 - 999"}
+            label={"Digite seu CEP"}
+            fullWidth
+            variant={"outlined"}
+          />
+
+          <Typography color={"error"}>CEP inválido</Typography>
+
+          <Button
+            variant={"contained"}
+            color={"secondary"}
+            sx={{ width: "220px" }}
+          >
+            Buscar
+          </Button>
+        </FormElementsContainer>
+
+        <ProfissionaisPaper>
+          <ProfissionaisContainer>
+            <UserInformation
+              picture={"https://github.com/dsrivan.png"}
+              name={"Ivan Rosa"}
+              rating={4}
+              description={"Cosmorama"}
+            />
+            <UserInformation
+              picture={"https://github.com/dsrivan.png"}
+              name={"Ivan Rosa"}
+              rating={4}
+              description={"Cosmorama"}
+            />
+            <UserInformation
+              picture={"https://github.com/dsrivan.png"}
+              name={"Ivan Rosa"}
+              rating={4}
+              description={"Cosmorama"}
+            />
+            <UserInformation
+              picture={"https://github.com/dsrivan.png"}
+              name={"Ivan Rosa"}
+              rating={4}
+              description={"Cosmorama"}
+            />
+            <UserInformation
+              picture={"https://github.com/dsrivan.png"}
+              name={"Ivan Rosa"}
+              rating={4}
+              description={"Cosmorama"}
+            />
+            <UserInformation
+              picture={"https://github.com/dsrivan.png"}
+              name={"Ivan Rosa"}
+              rating={4}
+              description={"Cosmorama"}
+            />
+          </ProfissionaisContainer>
+        </ProfissionaisPaper>
+      </Container>
     </div>
   );
 }
